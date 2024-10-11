0ed1a10e621f7dfa27041688b42c42dbd8b0401f
IntepeZafer <intepezafer@gmail.com>
2024-10-11 23:47:14 +0300

    Format Üzerinden Alınan Değer Api'ye Gönderilme İşlemi Tamamlandı

--- vite-project/public/SearchHeader/SearchHeader.jsx
+++ vite-project/public/SearchHeader/SearchHeader.jsx
@@ -1,16 +1,21 @@
+import { useState } from 'react';
 import './SearchHeaderStyle.css'
 import PropTypes from 'prop-types';
 
 const SearchHeader = ({search}) => {
-    const handleFormSubmit = (event) => {
-        search("Zafer İntepe");
-        event.preventDefault();
-    }
+  const [valueInput, setValue] = useState('');
+  const handleFormSubmit = (event) => {
+      search(valueInput);
+      event.preventDefault();
+  }  
+  const handleChange = (event) => {
+      setValue(event.target.value);
+  }
   return (
     <div className="formContainer">
         <form onSubmit={handleFormSubmit}>
             <label>Ne Arıyorsunuz?</label>
-            <input type="text" />
+            <input type="text" value={valueInput} onChange={handleChange}/>
         </form>
     </div>
   )
