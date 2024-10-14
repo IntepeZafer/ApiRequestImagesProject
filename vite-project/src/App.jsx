539cafd1cdc243d62ceb9826addba0e73e285785
Zafer İntepe <108065413+IntepeZafer@users.noreply.github.com>
2024-10-14 23:58:41 +0300

    Form Etiketinin Css Düzenlemesi Yapıldı

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css'
+import SearchHeader from '../Public/SearchHeader/SearchHeader'
 function App() {
   return (
     <>
+      <div className='App'>
+        <SearchHeader />
+      </div>
     </>
   )
 }
